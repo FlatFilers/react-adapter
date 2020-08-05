f24be35f2f47467da28bfd2d407432b506a19b4d
iamjaredwalters <jared.w.walters@gmail.com>
2020-08-05 11:56:30 -0700

    feat: add packages and config for packages

---
+++ webpack.dev.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.js');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  // serve source files
+  entry: './src/dev.index.tsx',
+});

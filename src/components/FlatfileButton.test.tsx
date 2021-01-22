a8cc5d8f80299044ec012617302b8b0d0bcef5fd
Beau Collins <beau@collins.pub>
2021-01-22 11:55:01 -0800

    Use exported props

--- src/components/FlatfileButton.test.tsx
+++ src/components/FlatfileButton.test.tsx
@@ -5,18 +5,14 @@ import FlatfileImporter from '@flatfile/adapter';
 import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import * as React from 'react';
 
-import FlatfileButton from './FlatFileButton';
+import FlatfileButton, { FlatfileButtonProps } from './FlatFileButton';
 
 jest.mock('@flatfile/adapter');
-
-type Props = typeof FlatfileButton extends React.ComponentType<infer P>
-  ? P
-  : never;
 
 describe('<FlatfileButton settings={testSettings} />', () => {
   afterEach(cleanup);
 
-  const props: Props = {
+  const props: FlatfileButtonProps = {
     customer: { userId: '12345' },
     licenseKey: 'aa921983-4db2-4da1-a580-fbca0b1c75b2',
     settings: {

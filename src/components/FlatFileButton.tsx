fb70752d1d76367d389d30518293f7f6b63fbb9e
Max Blank <blank.maksym@gmail.com>
2021-02-19 11:16:09 -0600

    feat(adapter): close portal if onData is resolved with null

--- src/components/FlatFileButton.tsx
+++ src/components/FlatFileButton.tsx
@@ -24,7 +24,7 @@ export type FlatfileButtonProps = React.DetailedHTMLProps<
   onBeforeFetch?: (req: IBeforeFetchRequest) => IBeforeFetchResponse;
   onInteractionEvent?: (req: IInteractionEvent) => void;
   onCancel?: () => void;
-  onData?: (results: FlatfileResults) => Promise<string | void>;
+  onData?: (results: FlatfileResults) => Promise<string | void | null>;
   onRecordChange?: (
     data: ScalarDictionaryWithCustom,
     index: number
@@ -97,9 +97,9 @@ const FlatfileButton: FC<FlatfileButtonProps> = ({
   const dataHandler = (results: FlatfileResults) => {
     importer?.displayLoader();
     onData?.(results).then(
-      (optionalMessage?: string | void) =>
-        optionalMessage
-          ? importer?.displaySuccess(optionalMessage)
+      (optionalMessage) =>
+        optionalMessage !== null
+          ? importer?.displaySuccess(optionalMessage || undefined)
           : importer?.close(),
       (error: Error | string) =>
         importer
